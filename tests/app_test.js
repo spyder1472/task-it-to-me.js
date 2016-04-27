6e6d41ca8163a553305ae20c3f71a9e23b6d2543
Kane Baccigalupi <kane.baccigalupi@gsa.gov>
2016-04-27 16:57:59 -0700

    Fixes bad command bug in the projects menu
            When this was fixed it exposed another bug where parts of the
            edit project code were asking too frequently for input and
            looping.
    
            There is still likely a bug related to the tasks menu doing
            similar. I am going to create a test and fix if necessary in the
            next commit.

--- tests/app_test.js
+++ tests/app_test.js
@@ -275,3 +275,14 @@ test('going back to the projects menu', function(test) {
 
   testStreams.mockInput(['a', 'House work', 'e', 'House work', 'b', 'ls', 'q']);
 });
+
+test('bug: app hangs at unknown command', function(test) {
+  setup();
+  test.plan(1);
+
+  app.run(function() {
+    test.match(testStreams.plainOutput(), "Unknown command: 'jj'");
+  });
+
+  testStreams.mockInput(['jj', 'q']);
+});
